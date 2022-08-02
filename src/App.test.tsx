281a1d35fa2c55c53883ee43c7aa9533d0a9c4ca
Eugen Kakareka <evv.kakareka@gmail.com>
2022-08-02 17:39:39 +0300

    change test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,25 +1,8 @@
-import React from "react";
-import { render, screen, fireEvent, act } from "@testing-library/react";
-import App from "./App";
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-describe("App", () => {
-  it("renders App component", () => {
-    render(<App />);
-    expect(screen.getByText(/The Batman/i)).toBeInTheDocument();
-    expect(screen.getByAltText(/The Batman/i)).toBeInTheDocument();
-  });
-});
-
-describe("Click in App", () => {
-  it("it should click on favorite", () => {
-    const { getAllByRole } = render(<App />);
-    const likes = getAllByRole("checkbox");
-    likes.forEach((like) => {
-      expect(like).not.toBeChecked();
-      act(() => {
-        fireEvent.click(like);
-      });
-      expect(like).toBeChecked();
-    });
-  });
+test('render div element', () => {
+  render(<App />);
+  expect(screen.getByText('react app')).toBeInTheDocument();
 });
