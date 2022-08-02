6a7a8502fb6fd7bc0de913fa14c2676df4cb85a4
Eugen Kakareka <evv.kakareka@gmail.com>
2022-08-02 17:42:56 +0300

    test fix

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('render div element', () => {
+test("render div element", () => {
   render(<App />);
-  expect(screen.getByText('react app')).toBeInTheDocument();
+  expect(screen.getByText("react app")).toBeInTheDocument();
 });
