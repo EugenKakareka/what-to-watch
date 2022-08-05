6161dbf6d8579d286baf68306a9ed77191b07f12
Eugen Kakareka <evv.kakareka@gmail.com>
2022-08-05 13:45:00 +0300

    deploy test

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { ThemeProvider } from "@mui/material";
-import { store } from "./store/store";
-import { theme } from "./theme/theme";
-import { Header } from "./components/Header/Header";
-import { Router } from "./routes/Router";
+// import { BrowserRouter } from "react-router-dom";
+// import { Provider } from "react-redux";
+// import { ThemeProvider } from "@mui/material";
+// import { store } from "./store/store";
+// import { theme } from "./theme/theme";
+// import { Header } from "./components/Header/Header";
+// import { Router } from "./routes/Router";
 import "./App.scss";
 
 function App() {
   return (
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <BrowserRouter>
-          <Header />
-          <Router />
-        </BrowserRouter>
-      </ThemeProvider>
-    </Provider>
+
+    <div> react App</div>
+    // <Provider store={store}>
+    //   <ThemeProvider theme={theme}>
+    //     <BrowserRouter>
+    //       <Header />
+    //       <Router />
+    //     </BrowserRouter>
+    //   </ThemeProvider>
+    // </Provider>
   );
 }
 
