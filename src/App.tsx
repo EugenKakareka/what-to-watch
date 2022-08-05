6ddf637278d0bd126c0f7939dcbb738440d677af
Eugen Kakareka <evv.kakareka@gmail.com>
2022-08-05 13:57:27 +0300

    change app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import React from "react";
-// import { BrowserRouter } from "react-router-dom";
-// import { Provider } from "react-redux";
-// import { ThemeProvider } from "@mui/material";
-// import { store } from "./store/store";
-// import { theme } from "./theme/theme";
-// import { Header } from "./components/Header/Header";
-// import { Router } from "./routes/Router";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { ThemeProvider } from "@mui/material";
+import { store } from "./store/store";
+import { theme } from "./theme/theme";
+import { Header } from "./components/Header/Header";
+import { Router } from "./routes/Router";
 import "./App.scss";
 
 function App() {
   return (
-
-    <div> react App</div>
-    // <Provider store={store}>
-    //   <ThemeProvider theme={theme}>
-    //     <BrowserRouter>
-    //       <Header />
-    //       <Router />
-    //     </BrowserRouter>
-    //   </ThemeProvider>
-    // </Provider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Header />
+          <Router />
+        </BrowserRouter>
+      </ThemeProvider>
+    </Provider>
   );
 }
 
